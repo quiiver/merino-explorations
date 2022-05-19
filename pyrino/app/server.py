a588e53db524c7f7abe3ca47f402dfeb36fe4a93
Wil Stuckey <wstuckey@mozilla.com>
2022-05-19 17:22:27 -0500

    Add dockerflow package and logging

--- pyrino/app/server.py
+++ pyrino/app/server.py
@@ -3,10 +3,35 @@ from asyncio import gather
 from typing import Dict
 from sanic import Sanic
 from sanic.response import json
+from dockerflow.sanic import Dockerflow
 
 from app.providers import wikipedia, adm, base
 
-app = Sanic("pyrino")
+log_config = {
+    'version': 1,
+    'formatters': {
+        'json': {
+            '()': 'dockerflow.logging.JsonLogFormatter',
+            'logger_name': 'myproject'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'json'
+        },
+    },
+    'loggers': {
+        'request.summary': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+app = Sanic("pyrino", log_config=log_config)
+dockerflow = Dockerflow(app)
 
 @app.before_server_start
 async def main_start(_):
